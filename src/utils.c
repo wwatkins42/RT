de6cb48c549c7cbe3c324b03948ec9cce3943852
William WATKINS <wwatkins@e1r7p4.42.fr>
2016-03-09 11:29:55 +0100

    hex_vec3 modified

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 16:13:08 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/03/09 10:54:18 by wwatkins         ###   ########.fr       */
+/*   Updated: 2016/03/09 11:29:12 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,8 @@ t_vec3			hex_vec3(const int hex)
 {
 	t_vec3	ret;
 
-	ret.x = hex % 256;
-	ret.y = ((hex >> 8) % 256);
-	ret.z = ((hex >> 16) % 256);
-	ret.x /= 255;
-	ret.y /= 255;
-	ret.z /= 255;
+	ret.x = (hex % 256) / 255.0;
+	ret.y = ((hex >> 8) % 256) / 255.0;
+	ret.z = ((hex >> 16) % 256) / 255.0;
 	return (ret);
 }
