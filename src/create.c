8077b989501c6f6556b03ab0f0abbcfbc3db83b4
Samuel COLLON <scollon@e1r7p2.42.fr>
2016-03-09 09:25:45 +0100

    camera.c -> create.c && light.c deleted

--- src/create.c
+++ src/create.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   camera.c                                           :+:      :+:    :+:   */
+/*   create.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scollon <scollon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 15:34:54 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/03/08 10:38:06 by scollon          ###   ########.fr       */
+/*   Updated: 2016/03/09 09:24:38 by scollon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,11 @@ static void	get_light_type(t_lgt *current, const char *line)
 		current->type = SPOT;
 	else if (ft_strstr(line, "DIRECTIONAL"))
 		current->type = DIRECTIONAL;
+	else
+		error(E_LTYPE, line, 0);
 }
 
-t_lgt		*create_light(t_env *e)
+t_lgt		*create_light(t_env *e, char *type)
 {
 	char	*line;
 	t_lgt	*current;
@@ -59,19 +61,41 @@ t_lgt		*create_light(t_env *e)
 	default_light(current);
 	while (get_next_line(e->arg.fd, &line) > 0 && ft_strlen(line) > 1)
 	{
-		if (ft_strstr(line, "- type: "))
-			get_light_type(current, line);
-		else if (ft_strstr(line, "pos: "))
+		get_light_type(current, type);
+		if (ft_strstr(line, "pos: "))
 			current->pos = parse_vector(line);
 		else if (ft_strstr(line, "dir: "))
 			current->dir = parse_vector(line);
 		else if (ft_strstr(line, "color: "))
-			current->color = hex_to_vec3(ft_atoi_base(ft_strstr(line, "0x"), 16));
+			current->color = hex_vec3(ft_atoi_base(ft_strstr(line, "0x"), 16));
 		else if (ft_strstr(line, "intensity: "))
 			current->intensity = ft_atof(ft_strstr(line, ":") + 1);
+		else
+			error(E_LPARAM, line, 0);
 		ft_strdel(&line);
 	}
 	ft_strdel(&line);
 	current->next = NULL;
 	return (current);
+}
+
+t_obj		*create_object(t_env *e, char *type)
+{
+	char	*line;
+	t_obj	*current;
+
+	if (!(current = t_lgt *)malloc(sizeof(t_lgt)))
+		error(E_MALLOC, NULL, 1);
+	default_object(current);
+	while (get_next_line(e->arg.fd, &line) > 0 && ft_strlen(line) > 1)
+	{
+		get_object_type(type);
+		if (ft_strstr(line, "pos: "))
+			current->pos = parse_vector(line);
+		else if (ft_strstr(line, "dir: "))
+			current->dir = parse_vector(line);
+		else if (ft_strstr(line, "color: "))
+			current->color = hex_vec3(ft_atoi_base(ft_strstr(line, "0x"), 16));
+		
+	}
 }
