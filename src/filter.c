d5a6604f1920e5720cf65d150ac778c26d633e2e
William WATKINS <wwatkins@e1r7p4.42.fr>
2016-03-14 10:42:35 +0100

    filters added

---
+++ src/filter.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filter.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/14 10:41:39 by wwatkins          #+#    #+#             */
+/*   Updated: 2016/03/14 10:42:11 by wwatkins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+void	img_iter(t_env *e, void (*f)(char *, char *, char *))
+{
+	unsigned int	len;
+	unsigned int	i;
+
+	i = 0;
+	len = e->current->img.sl * e->current->img.h;
+	if (e->current->img.img == NULL || f == NULL)
+		return ;
+	while (i < len)
+	{
+		if (i + 4 < len)
+			f(&e->current->img.img[i], &e->current->img.img[i + 1],
+				&e->current->img.img[i + 2]);
+		i += 4;
+	}
+}
+
+void	invert(char *r, char *g, char *b)
+{
+	*r = 255 - *r;
+	*g = 255 - *g;
+	*b = 255 - *b;
+}
