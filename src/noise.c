5b0bd89f8b54eb3359fd7968ec122b0aa4e2e7da
Samuel COLLON <scollon@e1r7p2.42.fr>
2016-03-17 09:14:28 +0100

    some improvement for texture generator

--- src/noise.c
+++ src/noise.c
@@ -6,7 +6,7 @@
 /*   By: scollon <scollon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/15 10:55:16 by scollon           #+#    #+#             */
-/*   Updated: 2016/03/16 11:02:33 by scollon          ###   ########.fr       */
+/*   Updated: 2016/03/17 09:12:34 by scollon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,12 @@ t_noise				init_noise_structure(int w, int h, int pas, int octave)
 
 static double		interpolate(double a, double b, double x)
 {
-	double value;
-
-	value = (1 - cos(x * M_PI)) / 2.0;
-	return (a * (1 - value) + b * value);
+	return (a * (1 - x) + b * x);
 }
 
 static double		get_value(t_noise *noise, int i, int j)
 {
-	return (noise->noise[i * noise->w_max + j]);
+	return (noise->noise[(i % noise->w_max) + (j % noise->h_max) * noise->w_max]);
 }
 
 static double		noise_generator(t_noise *n, double x, double y)
@@ -60,7 +57,7 @@ static double		noise_generator(t_noise *n, double x, double y)
 	return (interpolate(y1, y2, fmod(y / n->pas, 1)));
 }
 
-double				noise(t_noise *noise, double x, double y, double per)
+double				noise(t_noise *noise, double x, double y)
 {
 	int		i;
 	int		f;
@@ -75,8 +72,8 @@ double				noise(t_noise *noise, double x, double y, double per)
 	while (++i < noise->octave)
 	{
 		ret += p * noise_generator(noise, x * f, y * f);
-		p *= per;
+		p *= 0.6;
 		f *= 2;
 	}
-	return (ret * (1 - per) / (1 - p));
+	return (ret * (1 - 0.6) / (1 - p));
 }
--- src/texture_generator.c
+++ src/texture_generator.c
@@ -6,7 +6,7 @@
 /*   By: scollon <scollon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/16 11:25:19 by scollon           #+#    #+#             */
-/*   Updated: 2016/03/16 11:46:42 by scollon          ###   ########.fr       */
+/*   Updated: 2016/03/17 09:12:51 by scollon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,30 @@ t_vec3			noise_marble(t_noise *n, int x, int y)
 	double f;
 	t_vec3 color;
 
-	f = 1 - sqrt(fabs(sin(2 * PI * noise(n, x, y, 0.7))));
-	color.x = 1.0 * (1 - f) + 0.5 * f;
-	color.y = 1.0 * (1 - f) + 0.5 * f;
-	color.z = 1.0 * (1 - f) + 0.5 * f;
+	f = 1 - sqrt(fabs(sin(2 * PI * noise(n, x, y))));
+	color.x = 1.0 * (1 - f) + 0.7 * f;
+	color.y = 1.0 * (1 - f) + 0.7 * f;
+	color.z = 1.0 * (1 - f) + 0.7 * f;
+	return (color);
+}
+
+t_vec3			noise_wood(t_noise *n, int x, int y)
+{
+	double	f;
+	double	value;
+	t_vec3	color;
+	t_vec3	c1;
+	t_vec3	c2;
+
+	c1 = vec3(1.0, 0.99, 0.98);
+	c2 = vec3(0.2, 0.12, 0.02);
+	value = fmod(noise(n, x, y), 0.2);
+	if (value > 0.2 / 2)
+		value = 0.2 - value;
+	f = (1 - cos(PI * value / (0.2 / 2))) / 2;
+	color.x = c1.x * (1 - f) + c2.x * f;
+	color.y = c1.x * (1 - f) + c2.y * f;
+	color.z = c1.z * (1 - f) + c2.z * f;
 	return (color);
 }
 
@@ -29,26 +49,32 @@ t_vec3			noise_atom(t_noise *n, int x, int y)
 	double	f;
 	double	valeur;
 	t_vec3	color;
+	t_vec3	c1;
+	t_vec3	c2;
+	t_vec3	c3;
 
-	valeur = noise(n, x, y, 0.7);
+	c1 = vec3(0.45, 0.33, 0.49);
+	c2 = vec3(0.27, 0.0, 0.05);
+	c3 = vec3(0.03, 0.5, 0.73);
+	valeur = noise(n, x, y);
 	if(valeur <= 0.25)
-		color = (t_vec3) {1, 0 ,0};
+		color = c1;
 	else if(valeur < 0.5)
 	{
 		f = (valeur - 0.25) / (0.5 - 0.25);
-		color.x = 1 * (1 - f) + 0 * f;
-		color.y = 0 * (1 - f) + 1 * f;
-		color.z = 0 * (1 - f) + 0 * f;
+		color.x = c1.x * (1 - f) + c2.x * f;
+		color.y = c1.x * (1 - f) + c2.y * f;
+		color.z = c1.z * (1 - f) + c2.z * f;
 	}
 	else if(valeur < 0.75)
 	{
 		f = (valeur - 0.5) / (0.75 - 0.5);
-		color.x = 0 * (1 - f) + 0 * f;
-		color.y = 1 * (1 - f) + 0 * f;
-		color.z = 0 * (1 - f) + 1 * f;
+		color.x = c2.x * (1 - f) + c3.x * f;
+		color.y = c2.y * (1 - f) + c3.y * f;
+		color.z = c2.z * (1 - f) + c3.z * f;
 	}
 	else
-		color = (t_vec3) {0, 0, 1} ;
+		color = c3 ;
 	return (color);
 }
 
@@ -62,7 +88,7 @@ t_texture		texture_generator(int type, int width, int height)
 	text.w = width;
 	text.h = height;
 	text.defined = 1;
-	noise = init_noise_structure(width, height, width / 2, 10);
+	noise = init_noise_structure(width, height, 100, 7);
 	if (!(text.img = (t_vec3**)malloc(sizeof(t_vec3*) * height)))
 		error(E_MALLOC, NULL, 1);
 	y = -1;
@@ -73,8 +99,15 @@ t_texture		texture_generator(int type, int width, int height)
 			error(E_MALLOC, NULL, 1);
 		while (++x < width)
 		{
-			text.img[y][x] = type == 0 ? noise_atom(&noise, x, y) : noise_marble(&noise, x, y);
+			if (type == 0)
+				text.img[y][x] = noise_wood(&noise, x, y);
+			if (type == 1)
+				text.img[y][x] = noise_marble(&noise, x, y);
+			if (type == 2)
+				text.img[y][x] = noise_atom(&noise, x, y);
 		}
 	}
+	free(noise.noise);
+	noise.noise = NULL;
 	return (text);
 }
