08d881fd9f2a2a31047d9b64e8071e8e89ded6a3
William WATKINS <wwatkins@e1r7p4.42.fr>
2016-04-25 16:25:00 +0200

    texture rotation now adapted to spheres

--- src/texture.c
+++ src/texture.c
@@ -6,7 +6,7 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/16 15:01:43 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/04/25 16:06:43 by wwatkins         ###   ########.fr       */
+/*   Updated: 2016/04/25 16:23:13 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,8 @@ static t_vec3	texture_mapping_plane(t_obj *obj, t_vec3 **img, t_vec3 hit)
 	t_vec3	v_axis;
 
 	u_axis = vec3(obj->normal.y, obj->normal.z, -obj->normal.x);
-	vec3_rotate(&u_axis, vec3_fmul(obj->dir, obj->mat.texture.rotation));
+	if (obj->mat.texture.rotation != 0)
+		vec3_rotate(&u_axis, vec3_fmul(obj->dir, obj->mat.texture.rotation));
 	v_axis = vec3_cross(u_axis, obj->normal);
 	u = vec3_dot(hit, u_axis) * obj->mat.texture.scale;
 	v = vec3_dot(hit, v_axis) * obj->mat.texture.scale;
@@ -64,6 +65,8 @@ static t_vec3	texture_mapping_sphere(t_obj *obj, t_vec3 **img, t_vec3 hit)
 	int		j;
 
 	d = vec3_norm(vec3_sub(hit, obj->pos));
+	if (obj->mat.texture.rotation != 0)
+		vec3_rotate(&d, vec3_fmul(obj->dir, obj->mat.texture.rotation));
 	u = 0.5 + atan2(d.z, d.x) / M_PI * 0.5;
 	v = 0.5 - asin(d.y) / M_PI;
 	if (obj->mat.texture.filtering)
